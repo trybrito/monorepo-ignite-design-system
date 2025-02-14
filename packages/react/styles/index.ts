743d71e8ad502f936b17b63d019e05d9ee32b68d
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-13 22:12:27 -0300

    chore: stitches

---
+++ packages/react/styles/index.ts
@@ -0,0 +1,37 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  globalCss,
+  getCssText,
+  css,
+  keyframes,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    radii,
+    space,
+  },
+})
