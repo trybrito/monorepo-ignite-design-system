646fefde2c1c5101bc0d2bf649579f8dbde82ee0
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-16 01:27:45 -0300

    styles + docs: TextInput

---
+++ packages/react/src/components/TextInput/styles.ts
@@ -0,0 +1,48 @@
+import { styled } from '../../styles'
+
+export const TextInputContainer = styled('div', {
+  backgroundColor: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray900',
+  display: 'flex',
+  alignItems: 'baseline',
+
+  '&:has(input:focus)': {
+    borderColor: '$ignite300',
+  },
+
+  '&:has(input:disabled)': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+})
+
+export const Prefix = styled('span', {
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$regular',
+  color: '$gray400',
+})
+
+export const Input = styled('input', {
+  fontFamily: '$default',
+  fontSize: '$sm',
+  border: 0,
+  color: '$white',
+  fontWeight: 'regular',
+  background: 'transparent',
+
+  '&:focus': {
+    outline: 0,
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
