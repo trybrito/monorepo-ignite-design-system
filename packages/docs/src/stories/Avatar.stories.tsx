c09c67ae2b0faba6353807d905fbdf1bc7705818
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-16 00:24:27 -0300

    styles + docs: Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/trybrito.png',
+    alt: 'Thiago Brito',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
