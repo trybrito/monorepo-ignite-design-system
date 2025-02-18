35a5cea1eb9677b078b92f821abaae28177ee3ff
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-17 23:54:07 -0300

    fix: change package name and imports

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@try-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
