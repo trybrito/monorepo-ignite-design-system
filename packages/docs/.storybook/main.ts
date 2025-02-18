77be454bf17c1be62d4d5f19012f60495561fdde
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-17 22:44:05 -0300

    fix: add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,5 +25,12 @@ const config: StorybookConfig = {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/monorepo-ignite-design-system'
+    }
+
+    return config
+  },
 }
 export default config
